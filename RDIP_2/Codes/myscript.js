dd90691ecfae14a4d832392f5381329b27ceefb9
Pragathi S <pragathis1026@gmail.com>
2020-07-17 13:21:58 +0530

    Adding Corpus

---
+++ RDIP_2/Codes/myscript.js
@@ -0,0 +1,14 @@
+/* Corpus */
+
+var sentence = '{"English":['+
+                    '{"a":"The child liked the chocolate.", "b":"She was stopped by the bravest knight.", "c":"Mary baked a cake for his birthday",' + 
+                        '"d":"She decorated the cake carefully", "5":"Mary wore a dress with polka dots."}],' +
+
+                '"Hindi":[' +
+                    '{"a":"राम ने सीता के लिए फल तोड़ा।", "b":"छोटे बच्चे पाठशाला जल्दी आयेंगे।", "c":"मेहनत का फल मीठा होता है।", "d":"वाह! वह खूबसूरत है।",' +
+                        '"e":"पेड़ से पत्ते गिर गए।"}]}';
+
+/*
+    var obj = JSON.parse(sentence);
+    alert(obj.English[0].a);
+*/
